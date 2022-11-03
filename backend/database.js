7eb35843e61e92b04cdf0438d94e8ec31aaac80b
CodeKhushal <khushalsachdeva22@gmail.com>
2022-11-03 21:27:00 +0530

    Features added

--- backend/database.js
+++ backend/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host : 'localhost',
     user :'root',
     database :'khushal',
-    password : 'root',
+    password : '25072002',
     // port :5857
 })
 module.exports = pool.promise();
